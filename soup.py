78c957ae9db4ec7d1a85577f8b2a6ea5ed1a6250
Ryan Blachowicz <ryansblachowicz@gmail.com>
2019-09-21 12:26:07 -0400

    Prints text of article

--- soup.py
+++ soup.py
@@ -2,16 +2,23 @@ from bs4 import BeautifulSoup
 import requests
 import urllib
 
-with open('sample.html') as html_file:
+with open('sample2.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-title = soup.title.string
-caption = soup.h2.string
-body = soup.p.string
 
-print(title)
-print(caption)
 
-table = soup.find_all('div',attrs={"class":"body"})
-for x in table:
-    print(x.find('p'))
+#deletes extra text
+filed = soup.find_all("p", attrs={"class":"sr-only c-entry-box--compact__labels-title"})
+for i in filed:
+    i.decompose()
+
+#prints article and adds quotes around caption
+counter = 0
+content = soup.find_all("p")
+print(soup.h1.text)
+for i in content:
+    if counter != 0:
+        print(i.text, "\n")
+    if counter == 0:
+        print("\n" + "\"" + i.text + "\"" + "\n")
+        counter += 1
